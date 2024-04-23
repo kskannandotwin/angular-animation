9b691178f901e689ec2bafe1f11f0bfa49b21ef5
kannanATexcelforte <kannan@excelforte.com>
2024-04-23 21:35:06 +0530

    23.04.2024-hpsitacs
    completed

---
+++ src/app/components/course/course.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Course } from 'src/app/models/course.model';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+
+  @Input() course!: Course;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
