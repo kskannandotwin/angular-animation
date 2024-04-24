2f800edb1ad8326665145dd49565e7e4e7f400c3
kannanATexcelforte <kannan@excelforte.com>
2024-04-24 16:33:32 +0530

    24.04.2024-nsdvesd
    completed

--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -5,7 +5,7 @@ import { Course } from 'src/app/models/course.model';
   selector: 'app-course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css'],
-  encapsulation: ViewEncapsulation.Emulated
+  encapsulation: ViewEncapsulation.None
 })
 export class CourseComponent implements OnInit {
 
