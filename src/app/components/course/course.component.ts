660d1480f673bfe35ad9467760394661d7d86948
kannanATexcelforte <kannan@excelforte.com>
2024-04-24 16:15:59 +0530

    24.04.2024-esdfss
    completed

--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Course } from 'src/app/models/course.model';
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  // styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class CourseComponent implements OnInit {
 
