9963c06a76434375e4e2ebbda832959a2d94d9ed
kannanATexcelforte <kannan@excelforte.com>
2024-05-03 16:16:34 +0530

    03.05.2024-htdsaaussta
    completed

--- src/app/animation/animation.component.ts
+++ src/app/animation/animation.component.ts
@@ -6,15 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./animation.component.css'],
   animations: [
     trigger('clickedState', [
-      
+      state('default', style({
+        width: '50px',
+        height: '100px',
+        backgroundColor: 'orange'
+      })),
+      state('clicked', style({
+        width: '100px',
+        height: '50px',
+        backgroundColor: 'blue'
+      })),
+      transition('default => clicked', animate('300ms ease-in')),
     ])
   ]
 })
 export class AnimationComponent implements OnInit {
 
+  clickInfo = 'default';
+
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.clickInfo = 'clicked';
+    }, 3000); // 2 seconds       
   }
-
 }
