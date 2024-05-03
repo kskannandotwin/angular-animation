d12ce23174aa36179c9c3738a51d0235050647a7
kannanATexcelforte <kannan@excelforte.com>
2024-05-03 21:52:22 +0530

    03.05.2024-htamt
    completed

--- src/app/animation/animation.component.ts
+++ src/app/animation/animation.component.ts
@@ -11,24 +11,46 @@ import { Component, OnInit } from '@angular/core';
         height: '100px',
         backgroundColor: 'orange'
       })),
+      state('mousedown', style({
+        backgroundColor: 'red',
+        border: '1px solid black',
+        width: '100px',
+        height: '100px'
+      })),
       state('clicked', style({
         width: '100px',
         height: '50px',
         backgroundColor: 'blue'
       })),
-      transition('default => clicked', animate('300ms ease-in')),
+      transition('default => clicked', animate('1s 200ms ease-in')),
+      transition('clicked => default', animate(300)),
+      transition('mousedown <=> clicked', animate(300)),
+      transition('default <=> mousedown', animate(300))
     ])
   ]
 })
 export class AnimationComponent implements OnInit {
 
   clickInfo = 'default';
+  paragraphState = 'default';
 
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  onClickSample() {
+    this.clickInfo = 'default';
     setTimeout(() => {
       this.clickInfo = 'clicked';
-    }, 3000); // 2 seconds       
+    }, 3000);
+  }
+
+  onParagraphClick() {
+    this.paragraphState = 'default';
+    setTimeout(() => {
+      this.paragraphState = 'mousedown';
+    }, 3000);
   }
 }
