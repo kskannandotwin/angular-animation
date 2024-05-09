cd3ac7bc44e23b01b144062dfc4bccfee8597590
kannanATexcelforte <kannan@excelforte.com>
2024-05-09 16:50:28 +0530

    09.05.2024-aapuab
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AnimationBuilder, animate, style } from '@angular/animations';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
@@ -6,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  constructor(private builder: AnimationBuilder) { }
+
+  animate(element: HTMLElement) {
+    const animation = this.builder.build([
+      style({
+        backgroundColor: 'blue',
+        width: '200px',
+      }),
+      animate(2000, style({
+        backgroundColor: 'red',
+        width: '500px',
+      })),
+      animate(300)
+    ]);
+    const player = animation.create(element);
+    player.play();
+  }
 }
